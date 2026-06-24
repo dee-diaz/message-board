07489ab1f8056b9a200909768f7e46e3b1ebc537
dee-diaz <diazthediablo@yahoo.com>
2026-06-24 16:57:18 -0300

    feat: render messages from data with formatted dates and own-message styling

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,9 +4,31 @@ const chat_get = (req, res) => {
   if (!req.session.userName) {
     return res.redirect('/');
   }
-  res.render('chat', { userName: req.session.userName, messages });
+  const fMessages = messages.map((message) => {
+    return {
+      user: message.user,
+      text: message.text,
+      added: formatDateAndTime(message.added),
+    };
+  });
+  res.render('chat', { userName: req.session.userName, fMessages });
 };
 
 module.exports = {
   get: chat_get,
 };
+
+function formatDateAndTime(date) {
+  const fTime = date.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+
+  const fDate = date.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+  });
+
+  return `${fTime} • ${fDate}`;
+}
